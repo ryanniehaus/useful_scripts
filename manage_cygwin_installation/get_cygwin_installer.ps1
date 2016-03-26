9d2f01357bad400691ffb93071a1ace27b50356b
Ryan Niehaus <ryan.niehaus@gmail.com>
2016-03-26 13:38:00 -0500

    Added scripts for managing cygwin install

---
+++ manage_cygwin_installation/get_cygwin_installer.ps1
@@ -0,0 +1,1 @@
+wget https://cygwin.com/setup-x86_64.exe -OutFile setup-x86_64.exe
---
+++ manage_cygwin_installation/select_packages_for_cygwin_install.bat
@@ -0,0 +1,17 @@
+set CURRENT_BATCH_DIR=%~dp0%
+powershell -executionpolicy bypass -File .\get_cygwin_installer.ps1
+setup-x86_64.exe^
+ -R %CURRENT_BATCH_DIR%..^
+ -P packageList.txt^
+ -C categoryList.txt^
+ -a x86_64^
+ -W^
+ -q^
+ -l %CURRENT_BATCH_DIR%^
+ -n^
+ -d^
+ -g^
+ -o^
+ -A^
+ -Y^
+ -M
